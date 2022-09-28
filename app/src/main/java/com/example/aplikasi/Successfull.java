8afe9e402f871639f84a169327b32e26f69e8ab1
fardanmaula <fardanm.azizi11@gmail.com>
2022-09-28 22:17:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/aplikasi/Successfull.java
@@ -0,0 +1,13 @@
+package com.example.aplikasi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Successfull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successfull);
+    }
+}
